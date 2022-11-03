5ad83d51298cb5c0daa0eb7d24d9f41ed405214a
rafazolondek <rafaelzolondek@outlook.com>
2022-11-03 13:53:43 -0300

    desenvolvendo o index

--- Routes/routes.js
+++ Routes/routes.js
@@ -10,4 +10,12 @@ routes.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'login.html'));
 });
 
+routes.get('/servicos', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'servicos.html'));
+});
+
+routes.get('/sobre', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'sobre.html'));
+});
+
 module.exports = routes;
--- consulta.js
+++ consulta.js
@@ -1,10 +1,10 @@
 const db = require('./conexao');
 
-async function listarFuncionarios(){
+async function listarPessoas(){
     await db.connect()
-    result = await db.query('SELECT * FROM funcionario')
+    result = await db.query('SELECT * FROM pessoa')
     console.log(result.rows)
     await db.end()
 }
 
-listarFuncionarios();
+listarPessoas();
