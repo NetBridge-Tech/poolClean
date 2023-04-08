c76448efc2425c5e203ea8c2556362521f6363bd
Adrian Rafael Zolondek <71945361+rafazolondek@users.noreply.github.com>
2023-04-08 15:30:50 -0300

    criando consulto por ide instalando sequelize

--- conexao.js
+++ conexao.js
@@ -9,3 +9,14 @@ const client = new pg.Client({
 });
 
 module.exports = client;
+
+// const {Sequelize} = require('sequelize')
+// const sequelize = new Sequelize('exercicioSeq', 'postgres', 'postgres', {
+//     host: 'localhost',
+//     dialect: 'postgres'
+// });
+
+// module.exports = {
+//     Sequelize:Sequelize,
+//     sequelize:sequelize
+// }
--- consulta.js
+++ consulta.js
@@ -2,7 +2,7 @@ const db = require('./conexao');
 
 async function listarPessoas(){
     await db.connect()
-    result = await db.query('SELECT * FROM pessoa')
+    result = await db.query('SELECT nome, email FROM pessoa ORDER BY id DESC LIMIT 1')
     console.log(result.rows)
     await db.end()
 }
